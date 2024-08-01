6b3803d231d821485fb44c530d27122a0d7c0dc6
Liam O'Herlihy <loherlihy@csumb.edu>
2024-07-31 23:21:12 -0700

    Removed unused Toast.

--- app/src/main/java/com/daclink/fitjourney/MainActivity.java
+++ app/src/main/java/com/daclink/fitjourney/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
 
 import androidx.appcompat.app.AppCompatActivity;
