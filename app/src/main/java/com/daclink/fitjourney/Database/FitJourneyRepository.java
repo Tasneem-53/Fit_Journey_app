e98360a9b922903e5b64fe17123875292acf3313
Liam O'Herlihy <loherlihy@csumb.edu>
2024-07-31 23:20:00 -0700

    FitJourneyRepository Exercises + Meals

--- app/src/main/java/com/daclink/fitjourney/Database/FitJourneyRepository.java
+++ app/src/main/java/com/daclink/fitjourney/Database/FitJourneyRepository.java
@@ -1,51 +1,58 @@
 package com.daclink.fitjourney.Database;
 
 import android.app.Application;
-import android.util.Log;
+
 
 import com.daclink.fitjourney.Database.entities.Exercise;
-import com.daclink.fitjourney.MainActivity;
+import com.daclink.fitjourney.Database.entities.Meals;
 
-import java.util.ArrayList;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
+import java.util.List;
 
 public class FitJourneyRepository {
-    private FitJourneyDAO fitJourneyDAO;
-    private ArrayList<Exercise> allLogs;
+    private MealsDAO mealsDAO;
+    private ExerciseDAO exerciseDAO;
 
-    public FitJourneyRepository(Application application){
+    public FitJourneyRepository(Application application) {
         FitJourneyDatabase db = FitJourneyDatabase.getDatabase(application);
-        this.fitJourneyDAO = db.fitJourneyDAO();
-        this.allLogs = this.fitJourneyDAO.getAllRecords();
+        this.mealsDAO = db.mealsDAO();
+        this.exerciseDAO = db.exerciseDAO();
     }
 
-    public ArrayList<Exercise> getAllLogs(){
-        Future<ArrayList<Exercise>> future = FitJourneyDatabase.databaseWriteExecutor.submit(
-
-                new Callable<ArrayList<Exercise>>() {
-                    @Override
-                    public ArrayList<Exercise> call() throws Exception {
-                        return fitJourneyDAO.getAllRecords();
-                    }
-                }
-
-        );
-
-        try {
-            return future.get();
-        }catch (InterruptedException | ExecutionException e){
-            Log.i(MainActivity.TAG, "Problem when getting all ExerciseLogs in the repository");
-        }
-        return null;
-    }
-
-public void insertExercise(Exercise exercise){
-        FitJourneyDatabase.databaseWriteExecutor.execute(() ->
-        {
-            fitJourneyDAO.insert(exercise);
+    public void insertMeal(Meals meal) {
+        FitJourneyDatabase.databaseWriteExecutor.execute(() -> {
+            mealsDAO.insert(meal);
         });
-}
+    }
+
+    public List<Meals> getAllMeals() {
+        return mealsDAO.getAllMeals();
+    }
+
+    public void insertExercise(Exercise exercise) {
+        FitJourneyDatabase.databaseWriteExecutor.execute(() -> {
+            exerciseDAO.insert(exercise);
+        });
+    }
+
+    public List<Exercise> getAllExercises() {
+        return exerciseDAO.getAllExercises();
+    }
+
+    public void deleteAllExercises() {
+        FitJourneyDatabase.databaseWriteExecutor.execute(() -> {
+            exerciseDAO.deleteAll();
+        });
+    }
+
+    public List<Meals> getAllMealsOrderedByDate() {
+        return mealsDAO.getAllMealsOrderedByDate();
+    }
+
+    public void deleteAllMeals() {
+        FitJourneyDatabase.databaseWriteExecutor.execute(() -> {
+            mealsDAO.deleteAll();
+        });
+    }
+
 
 }
