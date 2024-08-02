dbc22d99fbcabbd9782842bb1159e8e882d0e8bb
valadez1114 <advaladez@csumb.edu>
2024-08-01 23:50:09 -0700

    Worked on the login activity.java file I wrote a method to validate username and password but its not working it still allows you to log in.

--- app/src/main/java/com/daclink/fitjourney/LoginActivity.java
+++ app/src/main/java/com/daclink/fitjourney/LoginActivity.java
@@ -5,14 +5,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 import com.daclink.fitjourney.databinding.ActivityLoginBinding;
 
-import java.net.PasswordAuthentication;
+import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
 private ActivityLoginBinding binding;
@@ -28,15 +25,38 @@ private String mPassword;
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, WelcomeUserActivity.class);
+
                 startActivity(intent);
             }
         });
+
+        binding.createNewAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean userExist = false;
+                //getting the edit text from username and password
+                mUserName = binding.usernameEditText.getText().toString();
+                mPassword = binding.passwordEditText.getText().toString();
+                if(!mUserName.isEmpty() && !mPassword.isEmpty()){
+                   userExist= verifyUserName(mUserName, mPassword);
+                }
+                System.out.println(userExist);
+                if(userExist == true){
+                Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
+                startActivity(intent);
+                }
+            }
+        });
+
     }
     //todo finish writing this method
     //this method will check with the database to see if user name exist
-    private boolean verifyUserName(String mUserName, String mPassword){
-        return false;
+    private boolean verifyUserName(String userName, String password){
+        return userName.equals("Adan") && (password.equals("123"));
     }
 
-
+    //Intent factory for LoginActivity
+    static Intent loginIntentFactory(Context context){
+        return new Intent(context, LoginActivity.class);
+    }
 }
