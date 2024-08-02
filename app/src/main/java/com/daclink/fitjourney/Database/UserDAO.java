7ae6c07434ea43bcc30f8a0826b5f70f4956e1f7
valadez1114 <advaladez@csumb.edu>
2024-08-01 21:25:58 -0700

    Updated UserDAO.java

--- app/src/main/java/com/daclink/fitjourney/Database/UserDAO.java
+++ app/src/main/java/com/daclink/fitjourney/Database/UserDAO.java
@@ -2,6 +2,7 @@ package com.daclink.fitjourney.Database;
 
 import androidx.room.Dao;
 import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 import java.util.List;
 import com.daclink.fitjourney.Database.entities.User;
@@ -9,13 +10,13 @@ import com.daclink.fitjourney.Database.entities.User;
 @Dao
 public interface UserDAO {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insert(User user);
 
     @Query("SELECT * FROM User WHERE username = :username")
     User getUserByUsername(String username);
 
-    @Query("SELECT * FROM User")
+    @Query("SELECT * FROM " + FitJourneyDatabase.USER_TABLE)
     List<User> getAllUsers();
 }
 
